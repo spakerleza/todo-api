da8947f02c2d9f315d8bc7dc274450b8d805b2f3
Sab Udeh Chukwumdimma <chukwumdimma4life@gmail.com>
2018-01-23 12:46:09 +0100

    Add get todos app

--- server/server.js
+++ server/server.js
@@ -21,9 +21,17 @@ app.post("/todos", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.get("/todos", (req, res) => {
+    Todo.find().then( todos => {
+        res.send({todos});
+    }, e => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3001, () => {
     console.log("Started on port 3000");
 });
 
-module.exports = {app};
+// module.exports = {app};
 
